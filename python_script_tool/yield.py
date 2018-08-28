842b10a7f5940e85eac118729ef51523b03da3b7
AirFishWang <1013812915@qq.com>
2018-08-28 15:38:35 +0800

    add softmax

--- python_script_tool/yield.py
+++ python_script_tool/yield.py
@@ -2,17 +2,23 @@
 import random
 
 
+def gen():
+    while True:
+        list = [1, 2, 3, 4]
+        random.shuffle(list)
+        for x in list:
+            yield x
+
+
 def test_yield():
-    def gen():
-        while True:
-            list = [1, 2, 3, 4]
-            random.shuffle(list)
-            for x in list:
-                yield x
     f = gen()
     for i in range(10):
         print f.next()
 
+    for x in f:
+        print x
+
 
 if __name__ == "__main__":
     test_yield()
+
