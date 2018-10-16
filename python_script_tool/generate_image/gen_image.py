839ab11652d759ae254986738a68caca2e63f8b9
AirFishWang <1013812915@qq.com>
2018-10-16 19:18:57 +0800

    rewrite gen_image.py

--- python_script_tool/generate_image/gen_image.py
+++ python_script_tool/generate_image/gen_image.py
@@ -14,7 +14,8 @@ imgaug.seed(random.randint(0, 100))
 class Generator():
     def __init__(self):
         self.fonts_dir = "./fonts"
-        self.fonts_list = ["arial.ttf", "huawenxihei.ttf", "huawensongti.ttf"]          # arial不能再图片上绘制中文
+        # self.fonts_list = ["arial.ttf", "huawenxihei.ttf", "huawensongti.ttf"]          # arial不能再图片上绘制中文
+        self.fonts_list = os.listdir(self.fonts_dir)
         self.augment = False
         self.seq = iaa.Sequential([
             # iaa.PerspectiveTransform(scale=0.02, keep_size=True),
@@ -28,39 +29,11 @@ class Generator():
             ])
         ], random_order=True)
 
-    def gen_word_wuhan(self):
-        number = random.randint(10000, 100000)
-        word = str(number) + ".00"
-        return word
+    def gen_word(self):
+        pass
 
-    def gen_word_xiamen(self):
-        random_type = random.randint(0, 6)
-        if random_type in [0, 1]:
-            number = random.randint(100, 1000)
-        elif random_type in [2, 3]:
-            number = random.randint(10000, 100000)
-        elif random_type in [4]:
-            number = random.randint(100000, 1000000)
-        elif random_type in [5]:
-            number = random.randint(1000000, 10000000)
-        else:
-            number = random.randint(1000000, 100000000)
-
-        # number = random.randint(0, 35000000)
-        word = str(number)
-        length = len(word)
-        stack_list = []
-        count = 0
-        for i in range(length-1, -1, -1):
-            # stack_list.append(word[i].decode("utf-8"))
-            stack_list.append(word[i])
-            count += 1
-            if count == 3 and i != 0:
-                stack_list.append(',')
-                count = 0
-
-        word = ''.join(stack_list)[::-1]
-        return word
+    def font_to_image(self, word):
+        pass
 
     def find_text_box(self, img):
         height, width = img.shape[:2]
@@ -88,20 +61,20 @@ class Generator():
         # h = bottom - top + 1
         return left, right, top, bottom
 
-    def font_to_image(self, word, type):
 
-        if type == "wuhan":
-            font_size = 13
-            height = 17
-            font = ImageFont.truetype(os.path.join(self.fonts_dir, self.fonts_list[0]), font_size)
-        elif type == "xiamen":
-            image = self.font_to_image_xiamen(word)
-            return image
-            font_size = 28
-            height = 32
-            font = ImageFont.truetype(os.path.join(self.fonts_dir, self.fonts_list[0]), font_size)
-        else:
-            raise Exception("the parameter 'type' must be wuhan or xiamen")
+class GeneratorWuHan(Generator):
+    def __init__(self):
+        Generator.__init__(self)
+
+    def gen_word(self):
+        number = random.randint(10000, 100000)
+        word = str(number) + ".00"
+        return word
+
+    def font_to_image(self, word):
+        font_size = 13
+        height = 17
+        font = ImageFont.truetype(os.path.join(self.fonts_dir, self.fonts_list[0]), font_size)
         img = Image.new("L", (300, 100), 255)
 
         draw = ImageDraw.Draw(img)
@@ -118,7 +91,7 @@ class Generator():
         width = left_padding + text_w + right_padding
         img = Image.new('L', (width, height), random.randint(240, 255))
         draw = ImageDraw.Draw(img)
-        draw.text((left_padding-left, top_padding-top), word, fill=0, font=font)
+        draw.text((left_padding - left, top_padding - top), word, fill=0, font=font)
 
         # convert to opencv image format
         img = np.array(img)
@@ -128,7 +101,41 @@ class Generator():
 
         return img
 
-    def font_to_image_xiamen(self, word):
+
+class GeneratorXiaMen(Generator):
+    def __init__(self):
+        Generator.__init__(self)
+
+    def gen_word(self):
+        random_type = random.randint(0, 6)
+        if random_type in [0, 1]:
+            number = random.randint(100, 1000)
+        elif random_type in [2, 3]:
+            number = random.randint(10000, 100000)
+        elif random_type in [4]:
+            number = random.randint(100000, 1000000)
+        elif random_type in [5]:
+            number = random.randint(1000000, 10000000)
+        else:
+            number = random.randint(1000000, 100000000)
+
+        # number = random.randint(0, 35000000)
+        word = str(number)
+        length = len(word)
+        stack_list = []
+        count = 0
+        for i in range(length - 1, -1, -1):
+            # stack_list.append(word[i].decode("utf-8"))
+            stack_list.append(word[i])
+            count += 1
+            if count == 3 and i != 0:
+                stack_list.append(',')
+                count = 0
+
+        word = ''.join(stack_list)[::-1]
+        return word
+
+    def font_to_image(self, word):
         xiamen_char = "./xiamen_char"
         image_dicts = {}
         for x in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',']:
@@ -148,24 +155,105 @@ class Generator():
         return result
 
 
+class GeneratorUniversal(Generator):
+    def __init__(self):
+        Generator.__init__(self)
+        self.augment = True
+        self.seq = iaa.Sequential([
+            # iaa.PerspectiveTransform(scale=0.02, keep_size=True),
+            iaa.SomeOf((2, 4), [                  # 每次使用0到3个方式增强
+                iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
+                iaa.Add((-5, 5)),
+                iaa.Sometimes(0.5, [
+                    iaa.Scale(2.0),
+                    iaa.Scale(0.5)
+                ]),
+                iaa.OneOf([
+                    iaa.AverageBlur(k=(1, 3)),
+                    iaa.MedianBlur(k=(1, 3)),
+                    iaa.GaussianBlur(sigma=iap.Uniform(0.01, 0.05)),
+                ]),
+                iaa.ContrastNormalization((0.5, 2.0), per_channel=0.5)
+            ])
+        ], random_order=True)
+
+    def gen_word(self):
+        power = random.randint(0, 10)   # [x, y]
+        number = random.randint(10**power, 10**(power+1))
+
+        random_type = random.randint(0, 1)
+        if random_type in [0]:
+            return str(number)
+        elif random_type in [1]:
+            word = str(number)
+            length = len(word)
+            stack_list = []
+            count = 0
+            for i in range(length - 1, -1, -1):
+                stack_list.append(word[i])
+                count += 1
+                if count == 3 and i != 0:
+                    stack_list.append(',')
+                    count = 0
+
+            word = ''.join(stack_list)[::-1]
+            if random.randint(0, 2) == 0:
+                return word
+            else:
+                return word + "." + str(random.randint(0, 1000))
+
+    def font_to_image(self, word):
+        font_size = 28
+        height = 32
+        font = ImageFont.truetype(os.path.join(self.fonts_dir, random.choice(self.fonts_list)), font_size)
+        img = Image.new("L", (300, 100), 255)
+
+        draw = ImageDraw.Draw(img)
+        try:
+            draw.text((0, 0), word, fill=0, font=font)
+        except:
+            print word
+            exit()
+        # img.show()
+        left, right, top, bottom = self.find_text_box(np.array(img))
+        text_w = right - left + 1
+
+        left_padding = 4
+        right_padding = 7
+        top_padding = 3
+        bottom_padding = 4
+
+        width = left_padding + text_w + right_padding
+        img = Image.new('L', (width, height), random.randint(200, 255))
+        draw = ImageDraw.Draw(img)
+        draw.text((left_padding - left, top_padding - top), word, fill=0, font=font)
+
+        # convert to opencv image format
+        img = np.array(img)
+
+        if self.augment:
+            img = self.seq.augment_image(img)
+
+        return img
+
+
 def gen_sample(num, type, output_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    generator = Generator()
-
     if type == "wuhan":
-        label_name = "./wuhan.txt"
-        word_gen_handler = generator.gen_word_wuhan
+        generator = GeneratorWuHan()
     elif type == "xiamen":
-        label_name = "./xiamen.txt"
-        word_gen_handler = generator.gen_word_xiamen
+        generator = GeneratorXiaMen()
+    elif type == "universal":
+        generator = GeneratorUniversal()
     else:
-        raise Exception("the parameter 'type' must be wuhan or xiamen")
+        raise Exception("error type")
 
+    label_name = output_dir + ".txt"
     with open(label_name, "w") as fwiter:
         for i in range(num):
-            word = word_gen_handler()
-            image = generator.font_to_image(word, type)
+            word = generator.gen_word()
+            image = generator.font_to_image(word)
             image_name = "{}.jpeg".format(i+1)
             cv2.imwrite(os.path.join(output_dir, image_name), image)
             fwiter.write("{}/{} {}\n".format(output_dir, image_name, word))
@@ -173,30 +261,7 @@ def gen_sample(num, type, output_dir):
 
 
 if __name__ == "__main__":
-    generator = Generator()
-
-
-    # # word = generator.gen_word_wuhan()
-    # word = generator.gen_word_xiamen()
-    #
-    # image = generator.font_to_Image(word)
-    # # image.show()
-    # # image.save("test.jpeg", "JPEG")
-    # cv2.imshow("image", image)
-    # cv2.waitKey()
-    # cv2.imwrite("test.jpeg", image)
-    #
-    # print generator.gen_word_xiamen()
-
-    # gen_sample(6000, "wuhan", "./wuhan_smaple")
-    gen_sample(6000, "xiamen", "./xiamen_smaple")
-
-    # generator.font_to_image_xiamen("1,023,456,789")
-
-    # word = "1,234,567"
-    # image = generator.font_to_image(word, "xiamen")
-    # cv2.imshow("word1", image)
-    #
-    # image = generator.font_to_image("123", "xiamen")
-    # cv2.imshow("word2", image)
-    # cv2.waitKey(0)
+    # gen_sample(20, "wuhan", "./wuhan_sample")
+    # gen_sample(20, "xiamen", "./xiamen_sample")
+    # gen_sample(100000, "universal", "./universal_sample_train")
+    gen_sample(20000, "universal", "./universal_sample_test")
