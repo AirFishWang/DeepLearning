b1986ec0f363d93430ade98dea4117f979e47912
AirFishWang <1013812915@qq.com>
2018-11-16 11:18:00 +0800

    code conventions

--- python_test/child_fun_test.py
+++ python_test/child_fun_test.py
@@ -15,15 +15,17 @@
     reference : https://blog.csdn.net/celte/article/details/38982673
 """
 
+
 def father_fun():
     x= [10]
-    def child_fun():
 
+    def child_fun():
         print "in child_fun x = {}".format(x[0])
         x[0] = 20
 
     child_fun()
     print "in father_fun x = ", x[0]
+
 
 if __name__ == "__main__":
     father_fun()
