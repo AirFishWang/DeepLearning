f5944c6646845d10a12dac2064e7dd07b2f15298
AirFishWang <1013812915@qq.com>
2018-12-06 16:45:33 +0800

    add cross_entropy

---
+++ tesorflow_test/cross_entropy.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     cross_entropy
+   Description :
+   Author :        wangchun
+   date：          18-12-6
+-------------------------------------------------
+   Change Activity:
+                   18-12-6:
+-------------------------------------------------
+"""
+import keras
+import tensorflow as tf
+import numpy as np
+
+target = np.array([[0.0, 0.0, 1.0], [0.0, 1.0, 0.0]]).reshape((2, 1, 3))
+output = np.array([[1.0, 1.0, 2.0], [2.0, 1.0, 1.0]]).reshape((2, 1, 3))
+output = np.array([[0.25, 0.25, 0.5], [0.5, 0.25, 0.25]]).reshape((2, 1, 3))
+
+target = tf.constant(target, name="target")
+output = tf.constant(output, name="output")
+
+result = keras.backend.categorical_crossentropy(target, output)
+
+with tf.Session() as sess:
+    tf.initialize_all_variables().run()
+    out = sess.run(result)
+    print(out)
