f987c59fef427c00e55a9e5e68ed111f3db7f78a
AirFishWang <1013812915@qq.com>
2019-02-14 20:27:55 +0800

    add info

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DeepLearning
 some note or code about DeepLearning
+
+a useful website: https://handong1587.github.io/index.html
--- tesorflow_test/graph_test.py
+++ tesorflow_test/graph_test.py
@@ -3,7 +3,6 @@ import tensorflow as tf
 import numpy as np
 
 g1 = tf.Graph()
-
 with g1.as_default():
     a = tf.constant(np.ones((1, 2, 3)), name="a")
     with tf.variable_scope("foo"):
@@ -15,11 +14,18 @@ with g2.as_default():
     b = tf.constant(np.ones((2, 3, 4)), name="b")
 
 
-with tf.Session(graph=g1) as sess:
+with tf.Session(graph=g1) as sess1:
     tf.initialize_all_variables().run()
-    print sess.run(a)
+    print sess1.run(a)
 
 
-with tf.Session(graph=g2) as sess:
+with tf.Session(graph=g2) as sess2:
     tf.initialize_all_variables().run()
-    print sess.run(b)
+    print sess2.run(b)
+
+
+g3 = tf.Graph()
+with g3.as_default():
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.4)
+    config = tf.ConfigProto(gpu_options=gpu_options, allow_soft_placement=True)
+    sess3 = tf.Session(config=config)
